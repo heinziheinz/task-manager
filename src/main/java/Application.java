e99b0ec2aa5591522369eea7611d3189e381dee7
johnnyRobot <beitschalbua@gmx.at>
2023-09-15 10:52:03 +0200

    save

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,5 +3,12 @@ public class Application {
     public static void main(String[] args) {
 
         System.out.println("Hallo World2");
+//        Task Manager
+
+//        Develop a task management application where users can create, update, and track tasks and Appointments. Tasks and Appointments have titles and descriptions Tasks have in addition a due date (the date until the task has to be completed) and statuses (pending, completed). Appointments have a start date and time and an end date and time. Users have profiles with names and roles.
+//
+//                - Create a function that creates an Appointment only, if there is not already another appointment at the same time slot and only if the date is not in the past
+//                - Create a function which retrieves all tasks and appointments of this month
+//                - Create a function which retrieves all tasks which are in the past and not completed
     }
 }
